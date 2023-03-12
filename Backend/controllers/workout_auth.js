5c4fdedb1ea2a0a89cebf3f2328cf13ca776bac0
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-03-12 10:54:00 -0500

    backend - updated

--- Backend/controllers/workout_auth.js
+++ Backend/controllers/workout_auth.js
@@ -1,10 +1,22 @@
-const { json } = require('express')
 const mongoose = require('mongoose')
 
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.secret, { expiresIn: '3d' })
+}
 
 const userLogin = async (req, res) => {
-  res.json('You have reached the login request')
+  const { email, password } = req.body
+
+  try {
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 const userSignUP = async (req, res) => {
@@ -12,7 +24,10 @@ const userSignUP = async (req, res) => {
 
   try {
     const user = await User.signup(email, password)
-    res.status(200).json({ email, user })
+
+    //token creation
+    const token = createToken(user._id)
+    res.status(200).json({ email, token })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
