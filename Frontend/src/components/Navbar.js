4692e8ffa437e91d4433535d74aaac067d5ba37c
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-03-12 23:51:16 -0500

    updated formating

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
           {!user && (
             <div>
               <Link to="/login">Login</Link>
-              <Link to="/signup">Sign up</Link>
+              <Link to="/signup">Signup</Link>
             </div>
           )}
         </nav>
